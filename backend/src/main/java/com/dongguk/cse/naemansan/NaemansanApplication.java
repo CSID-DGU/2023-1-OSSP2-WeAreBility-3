3e5a51a9ce276db2dc8bd8595522f532557ee4d0
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-04-29 14:47:48 +0900

    feat: Spring Initialization Setting

---
+++ backend/src/main/java/com/dongguk/cse/naemansan/NaemansanApplication.java
@@ -0,0 +1,13 @@
+package com.dongguk.cse.naemansan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaemansanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaemansanApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dongguk/cse/naemansan/NaemansanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongguk.cse.naemansan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaemansanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
