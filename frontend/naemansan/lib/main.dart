21cbce611874e1b6cb3c7b3668c2d09593ad829c
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-05-02 13:36:43 +0900

    feat : 로그인여부 체크

--- frontend/naemansan/lib/main.dart
+++ frontend/naemansan/lib/main.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:naemansan/screens/home_sccreen.dart';
+import 'package:naemansan/screens/login_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  // spalsh 시간 조절하기
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  runApp(const App());
+
+  // prefs 초기화
+  final prefs = await SharedPreferences.getInstance();
+  // 로그인 여부 확인
+  final isLoggedin = prefs.getBool('isLoggedIn') ?? false;
+
+  runApp(App(isLoggedin: isLoggedin));
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  // 로그인 여부 체크
+  final bool isLoggedin;
+  const App({super.key, required this.isLoggedin});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     FlutterNativeSplash.remove(); // 초기화가 끝나는 시점에 삽입
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      // 로그인 여부에 따라 화면 분기
+      home: isLoggedin ? const HomeScreen() : const LoginScreen(),
     );
   }
 }
