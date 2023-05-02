1090688ee28058e340ec2f96b1f97eae9b54bdc8
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-05-02 15:01:30 +0900

    chore : 로고이미지 정리

--- frontend/naemansan/lib/widgets/login_button.dart
+++ frontend/naemansan/lib/widgets/login_button.dart
@@ -13,13 +13,25 @@ class LoginBtn extends StatelessWidget {
         fixedSize: const Size(307, 50), // 버튼 크기
       ),
       onPressed: () {},
-      child: Text(
-        whatsLogin,
-        style: const TextStyle(
-          fontSize: 15,
-          color: Color(0xFF49454F),
-          fontWeight: FontWeight.w500,
-        ),
+      // 로고와 텍스트를 가로로 나열
+      child: Row(
+        // 로고와 텍스트를 가운데 정렬
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/kakao.png',
+            width: 18,
+            height: 16,
+          ),
+          Text(
+            whatsLogin,
+            style: const TextStyle(
+              fontSize: 15,
+              color: Color(0xFF49454F),
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
       ),
     );
   }
