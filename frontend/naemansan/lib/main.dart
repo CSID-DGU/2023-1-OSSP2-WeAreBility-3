97102e82d435d77bf44295597da37583b1e4c640
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-05-02 13:18:31 +0900

    feat : splash 화면

--- frontend/naemansan/lib/main.dart
+++ frontend/naemansan/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:naemansan/screens/home_sccreen.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const App());
 }
 
@@ -11,6 +14,7 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove(); // 초기화가 끝나는 시점에 삽입
     return const MaterialApp(
       home: HomeScreen(),
     );
