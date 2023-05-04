50dd02335c9a0e5ee99a8d64ad190dbcc15249d7
wjdgus000000000 <wjdgus000000000@gmail.com>
2023-05-04 09:53:46 +0900

    fix:메인화면 로그인 기능 있는 버전으로 수정, gitignore수정
    Former-commit-id: aa033bc98bc09bdf032a23a37d51f6bf53154f4f

--- frontend/naemansan/lib/main.dart
+++ frontend/naemansan/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk_talk.dart';
-import 'package:naemansan/screens/home_sccreen.dart';
 import 'package:naemansan/screens/login_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:naemansan/screens/screen_index.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   // 환경변수
@@ -19,43 +18,46 @@ void main() async {
   // prefs 초기화
   final prefs = await SharedPreferences.getInstance();
   // 로그인 여부 확인
-  final isLoggedin = prefs.getBool('isLoggedIn') ?? false;
+  //final isLoggedin = prefs.getBool('isLoggedIn') ?? false;
   KakaoSdk.init(nativeAppKey: "${dotenv.env['YOUR_NATIVE_APP_KEY']}");
 
-  runApp(MyApp(isLoggedin: isLoggedin));
   runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  // 로그인 여부 체크
-  final bool isLoggedin;
-  const MyApp({super.key, required this.isLoggedin});
+//---
+class App extends StatefulWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  bool isLogged = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLoginStatus();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isLogged = prefs.getBool('isLogged') ?? false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     FlutterNativeSplash.remove(); // 초기화가 끝나는 시점에 삽입
     return MaterialApp(
-      // 로그인 여부에 따라 화면 분기
-      home: isLoggedin ? const HomeScreen() : LoginScreen(),
-    );
-  }
-}
-
-//---
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
       title: '내가 만든 산책로',
       routes: {
+        '/': (context) => isLogged ? const IndexScreen() : LoginScreen(),
         '/index': (context) => const IndexScreen(),
-        //지도 하단바 확인
-        //'/index': (context) => const MapIndex(),
       },
-      initialRoute: '/index',
+      initialRoute: '/',
     );
   }
 }
