df1339011b2c41516487706c6f041b76b4d6f3c7
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-05-08 13:05:03 +0900

    fix: 충돌 파일에 대한 폴더 구조 수정

---
+++ AI/walking_path_similarity/testing.py
@@ -0,0 +1,15 @@
+from struct import *
+import struct
+
+# Example byte sequence
+byte_seq = b'\xb6\x02\x02?\x00\x00\x80@'
+a = struct.pack('f', 123.456 )
+print(a)
+b = struct.unpack('f', a)
+print(b)
+
+"""# Use struct.unpack() to extract the value as a float
+f = struct.unpack('<f', byte_seq)[0]
+
+# Print the extracted value...
+print(f)  # Output: 123.45600128173828"""
---
+++ frontend/naemansan/lib/tabs/map.dart
@@ -0,0 +1,11 @@
+//지도 페이지 Map())
+import 'package:flutter/material.dart';
+
+class Map extends StatelessWidget {
+  const Map({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('지도 페이지'));
+  }
+}
