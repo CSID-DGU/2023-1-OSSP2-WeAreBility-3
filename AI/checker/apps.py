d9012f97f361d030701fc4fb4027650730ce24d9
Hoin666 <101854332+Hoin666@users.noreply.github.com>
2023-05-11 12:05:51 +0900

    modul import 오류 해결하면 django로 올리기 완료

--- AI/checker/apps.py
+++ AI/checker/apps.py
@@ -1,6 +1,15 @@
-from django.apps import AppConfig
+"""from django.apps import AppConfig
 
 
 class CheckerConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "checker"
+    name = "checker" """
+from django.apps import AppConfig
+import html
+import pathlib
+import os
+import walking_path_similarity_execution
+
+class WebappConfig(AppConfig):
+    name = 'checker'
+    predictor = walking_path_similarity_execution.similarity_Checker()
--- AI/manage.py
+++ AI/manage.py
@@ -3,6 +3,7 @@
 import os
 import sys
 
+sys.path.append('AI\checker')
 
 def main():
     """Run administrative tasks."""
