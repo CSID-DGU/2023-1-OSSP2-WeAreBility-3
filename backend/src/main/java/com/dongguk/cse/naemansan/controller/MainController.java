1f8d818e1f8ca5b3039536d47da05ccd8f8664a6
Kang Woobin <woobin929@dgu.ac.kr>
2023-05-14 23:49:11 +0900

    feat : 푸시알림 초기 구현

---
+++ backend/src/main/java/com/dongguk/cse/naemansan/controller/MainController.java
@@ -0,0 +1,30 @@
+package com.dongguk.cse.naemansan.controller;
+
+import com.dongguk.cse.naemansan.dto.request.NotificationRequestDto;
+import com.dongguk.cse.naemansan.dto.request.RequestDto;
+import com.dongguk.cse.naemansan.service.FirebaseCloudMessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class MainController {
+    private final FirebaseCloudMessageService firebaseCloudMessageService;
+
+    @PostMapping("/api/fcm")
+    public ResponseEntity pushMessage(@RequestBody RequestDto requestDto) throws IOException {
+        System.out.println(requestDto.getTargetToken() + " "
+                + requestDto.getTitle() + " " + requestDto.getBody());
+
+        firebaseCloudMessageService.sendMessageTo(
+                requestDto.getTargetToken(),
+                requestDto.getTitle(),
+                requestDto.getBody());
+        return ResponseEntity.ok().build();
+    }
+}
