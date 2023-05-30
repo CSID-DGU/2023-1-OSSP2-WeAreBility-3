d5a7bdd57696ba3206eb715552c6dd083e505ab8
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-05-30 13:20:48 +0900

    feat: 상권, 기업광고 List 조회 구현

--- backend/src/main/java/com/dongguk/cse/naemansan/controller/AdminController.java
+++ backend/src/main/java/com/dongguk/cse/naemansan/controller/AdminController.java
@@ -5,7 +5,7 @@ import com.dongguk.cse.naemansan.dto.request.AdvertisementRequestDto;
 import com.dongguk.cse.naemansan.dto.request.ShopRequestDto;
 import com.dongguk.cse.naemansan.dto.response.AdvertisementDto;
 import com.dongguk.cse.naemansan.dto.response.ShopDto;
-import com.dongguk.cse.naemansan.service.AdminService;
+import com.dongguk.cse.naemansan.service.CommonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,45 +13,35 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
-    private final AdminService adminService;
+    private final CommonService commonService;
 
     @PostMapping("/shop")
     public ResponseDto<?> createShopProfile(@RequestBody ShopRequestDto requestDto) {
-        return new ResponseDto<Boolean>(adminService.createShopProfile(requestDto));
-    }
-
-    @GetMapping("/shop/{shopId}")
-    public ResponseDto<?> readShopProfile(@PathVariable Long shopId) {
-        return new ResponseDto<ShopDto>(adminService.readShopProfile(shopId));
+        return new ResponseDto<Boolean>(commonService.createShopProfile(requestDto));
     }
 
     @PutMapping("/shop/{shopId}")
     public ResponseDto<?> updateShopProfile(@PathVariable Long shopId, @RequestBody ShopRequestDto requestDto) {
-        return new ResponseDto<ShopDto>(adminService.updateShopProfile(shopId, requestDto));
+        return new ResponseDto<ShopDto>(commonService.updateShopProfile(shopId, requestDto));
     }
 
     @DeleteMapping("/shop/{shopId}")
     public ResponseDto<?> deleteShopProfile(@PathVariable Long shopId) {
-        return new ResponseDto<Boolean>(adminService.deleteShopProfile(shopId));
+        return new ResponseDto<Boolean>(commonService.deleteShopProfile(shopId));
     }
 
     @PostMapping("/enterprise")
     public ResponseDto<?> createEnterpriseProfile(@RequestBody AdvertisementRequestDto requestDto) {
-        return new ResponseDto<Boolean>(adminService.createAdvertisementProfile(requestDto));
-    }
-
-    @GetMapping("/enterprise/{enterpriseId}")
-    public ResponseDto<?> readEnterpriseProfile(@PathVariable Long enterpriseId) {
-        return new ResponseDto<AdvertisementDto>(adminService.readAdvertisementProfile(enterpriseId));
+        return new ResponseDto<Boolean>(commonService.createAdvertisementProfile(requestDto));
     }
 
     @PutMapping("/enterprise/{enterpriseId}")
     public ResponseDto<?> updateEnterpriseProfile(@PathVariable Long enterpriseId, @RequestBody AdvertisementRequestDto requestDto) {
-        return new ResponseDto<AdvertisementDto>(adminService.updateAdvertisementProfile(enterpriseId, requestDto));
+        return new ResponseDto<AdvertisementDto>(commonService.updateAdvertisementProfile(enterpriseId, requestDto));
     }
 
     @DeleteMapping("/enterprise/{enterpriseId}")
     public ResponseDto<?> deleteEnterpriseProfile(@PathVariable Long enterpriseId) {
-        return new ResponseDto<Boolean>(adminService.deleteAdvertisementProfile(enterpriseId));
+        return new ResponseDto<Boolean>(commonService.deleteAdvertisementProfile(enterpriseId));
     }
 }
--- backend/src/main/java/com/dongguk/cse/naemansan/dto/response/ShopDto.java
+++ backend/src/main/java/com/dongguk/cse/naemansan/dto/response/ShopDto.java
@@ -16,13 +16,15 @@ public class ShopDto {
     private String introduction;
     private String image_path;
     private PointDto location;
+    private Double radius;
 
     @Builder
-    public ShopDto(Long id, String name, String introduction, Image image, PointDto location) {
+    public ShopDto(Long id, String name, String introduction, Image image, PointDto location, Double radius) {
         this.id = id;
         this.name = name;
         this.introduction = introduction;
         this.image_path = image.getUuidName();
         this.location = location;
+        this.radius = radius;
     }
 }
