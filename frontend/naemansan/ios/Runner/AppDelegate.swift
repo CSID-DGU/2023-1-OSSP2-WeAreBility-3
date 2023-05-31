43717edba55a6f9eb2e24e13904a171d6b1360c4
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-06-01 02:28:53 +0900

    feat : 지도기능

--- frontend/naemansan/ios/Runner/AppDelegate.swift
+++ frontend/naemansan/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,8 +8,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyD0BdBC_R_O1XOJWeVZFO75acTdyc32PgY")
     GeneratedPluginRegistrant.register(with: self)
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
