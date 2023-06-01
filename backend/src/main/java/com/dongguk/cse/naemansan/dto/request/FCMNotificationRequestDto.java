8334813c908c51e88400971d3b1ec207b5d7b72f
Kang Woobin <woobin929@dgu.ac.kr>
2023-06-01 20:02:40 +0900

    feat : 안드로이드 푸시알림 연동 코드 추가

---
+++ backend/src/main/java/com/dongguk/cse/naemansan/dto/request/FCMNotificationRequestDto.java
@@ -0,0 +1,20 @@
+package com.dongguk.cse.naemansan.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class FCMNotificationRequestDto {
+    private Long targetUserId;
+    private String title;
+    private String body;
+
+    @Builder
+    public FCMNotificationRequestDto(Long targetUserId, String title, String body) {
+        this.targetUserId = targetUserId;
+        this.title = title;
+        this.body = body;
+    }
+}
