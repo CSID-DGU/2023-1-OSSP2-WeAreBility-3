dc6e3bc71aee0e4e504d96d0fa3e1c6f058751b5
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-06-04 04:32:41 +0900

    feat : 산책로 없을 시 예외처리

--- frontend/naemansan/lib/widgets/horizontal_slider.dart
+++ frontend/naemansan/lib/widgets/horizontal_slider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:naemansan/screens/map/naver_map_screen.dart';
 import 'package:naemansan/services/login_api_service.dart';
 import 'package:naemansan/widgets/main_card.dart';
 
@@ -67,22 +68,66 @@ class _HorizontalSliderState extends State<HorizontalSlider> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      scrollDirection: Axis.horizontal,
-      itemCount: slideItems.length,
-      itemExtent: 190, // Set the item height to 200
-
-      itemBuilder: (context, index) {
-        return CardWidget(
-          id: slideItems[index].id,
-          title: slideItems[index].title,
-          location: slideItems[index].location,
-          length: slideItems[index].length,
-          likes: slideItems[index].likes,
-          keywords: slideItems[index].keywords,
-          created_date: slideItems[index].created_date,
-        );
-      },
-    );
+    return slideItems.isNotEmpty
+        ? ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: slideItems.length,
+            itemExtent: 190, // Set the item height to 200
+            itemBuilder: (context, index) {
+              return CardWidget(
+                id: slideItems[index].id,
+                title: slideItems[index].title,
+                location: slideItems[index].location,
+                length: slideItems[index].length,
+                likes: slideItems[index].likes,
+                keywords: slideItems[index].keywords,
+                created_date: slideItems[index].created_date,
+              );
+            },
+          )
+        : Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text(
+                  '현재 위치에 등록된 산책로가 없습니다!',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black87,
+                  ),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const NaverMapScreen()),
+                    )
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      side: const BorderSide(color: Colors.black87),
+                    ),
+                  ),
+                  icon: const Icon(
+                    Icons.add,
+                    color: Colors.black87,
+                  ),
+                  label: const Text(
+                    '산책로 등록하러 가기',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
   }
 }
