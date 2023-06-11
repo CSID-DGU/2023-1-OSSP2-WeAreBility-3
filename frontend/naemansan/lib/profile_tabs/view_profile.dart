3f0426b779c26dadfbc191916434773991e290fa
wjdgus000000000 <wjdgus000000000@gmail.com>
2023-06-11 14:16:08 +0900

    feat: 상대방 프로필 조회

--- frontend/naemansan/lib/profile_tabs/view_profile.dart
+++ frontend/naemansan/lib/profile_tabs/view_profile.dart
@@ -1,15 +1,42 @@
+//상대방 프로필 조회
+//마이페이지와 레이아웃 동일 - 텍스트 버튼 기능은 x (시간 남으면 ...)
+
 import 'package:flutter/material.dart';
-import 'package:naemansan/screens/screen_index.dart';
+import 'package:naemansan/models/other_user_model.dart';
+import 'package:naemansan/models/traildetailmodel.dart';
+import 'package:naemansan/services/login_api_service.dart';
 
-/*
-import 'package:naemansan/profile_tabs/badges.dart';
-import 'package:naemansan/profile_tabs/my_reviews.dart';
-import 'package:naemansan/profile_tabs/profile_edit.dart';
-import 'package:naemansan/profile_tabs/settings.dart';
-*/
+class ViewProfile extends StatefulWidget {
+  final int userId;
+  const ViewProfile({Key? key, required this.userId}) : super(key: key);
 
-class View extends StatelessWidget {
-  const View({super.key});
+  @override
+  State<ViewProfile> createState() => _ViewProfileState();
+}
+
+class _ViewProfileState extends State<ViewProfile> {
+  TraildetailModel? trailDetail;
+  OtherUserModel? otherUser;
+  String imageUrl = "";
+
+  @override
+  void initState() {
+    super.initState();
+    fetchWriterProfile();
+  }
+
+  Future<void> fetchWriterProfile() async {
+    ApiService apiService = ApiService();
+    Map<String, dynamic>? data =
+        await apiService.getOtherUserProfile(widget.userId);
+    if (data != null) {
+      setState(() {
+        otherUser = OtherUserModel.fromJson(data);
+        imageUrl =
+            'https://ossp.dcs-hyungjoon.com/image?uuid=${otherUser!.imagePath}';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,33 +48,18 @@ class View extends StatelessWidget {
         backgroundColor: Colors.white,
         title: Row(
           children: [
-            IconButton(
-              icon: const Icon(
-                Icons.arrow_back_ios_outlined,
-                color: Colors.black,
-              ),
-              onPressed: () {
-                //arrow 아이콘 클릭 시 홈 화면으로 이동 (홈 화면에 화살표 생김)
-                false;
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => const IndexScreen(),
-                  ),
-                );
-              },
-            ),
             Padding(
               padding: const EdgeInsets.only(left: 5.0),
               child: Row(
-                children: const [
+                children: [
                   Text(
-                    '000님의 프로필', //추가 작업 필요
-                    style: TextStyle(
+                    '${otherUser?.name ?? "Loading..."}님의 프로필',
+                    style: const TextStyle(
                       fontSize: 21,
                       fontWeight: FontWeight.w600,
                     ),
                   ),
-                  SizedBox(width: 8),
+                  const SizedBox(width: 8),
                 ],
               ),
             ),
@@ -57,7 +69,154 @@ class View extends StatelessWidget {
                 Icons.person_add_alt_1,
                 color: Colors.black,
               ),
-              onPressed: () {},
+              onPressed: () {}, //팔로우 신청
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(imageUrl),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    otherUser?.name ?? 'Loading...',
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    otherUser?.introduction ?? 'No Introduction',
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 16),
+                  const Divider(),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          const Text(
+                            '팔로워',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              otherUser?.followerCnt.toString() ?? 'Loading...',
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            '팔로잉',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 8),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              otherUser?.followingCnt.toString() ??
+                                  'Loading...',
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  const Divider(),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          const Text(
+                            '좋아요한 산책로',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 8),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              otherUser?.likeCnt.toString() ?? 'Loading...',
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            '작성한 후기',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 8),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              otherUser?.cmtCnt.toString() ?? 'Loading...',
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const Text(
+                            '획득한 뱃지',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 8),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              otherUser?.badgeCnt.toString() ?? 'Loading...',
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
