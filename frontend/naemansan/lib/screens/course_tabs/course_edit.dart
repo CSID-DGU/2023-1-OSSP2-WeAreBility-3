7cbf6f9f96f291830d9106062510f92fd3203307
wjdgus000000000 <wjdgus000000000@gmail.com>
2023-06-14 15:56:13 +0900

    feat: 등록한 산책로 수정 기능 완료

--- frontend/naemansan/lib/screens/course_tabs/course_edit.dart
+++ frontend/naemansan/lib/screens/course_tabs/course_edit.dart
@@ -25,6 +25,68 @@ class _CourseEditpageState extends State<CourseEditpage> {
 
   String newTitle = '';
   String newIntro = '';
+  List<String> allKeywords = [
+    '강남구',
+    '강동구',
+    '강북구',
+    '강서구',
+    '관악구',
+    '광진구',
+    '구로구',
+    '금천구',
+    '노원구',
+    '도봉구',
+    '동대문구',
+    '동작구',
+    '마포구',
+    '서대문구',
+    '서초구',
+    '성동구',
+    '성북구',
+    '송파구',
+    '양천구',
+    '영등포구',
+    '용산구',
+    '은평구',
+    '종로구',
+    '중구',
+    '중랑구',
+    '서울숲',
+    '경복궁',
+    '성수',
+    '한옥마을',
+    '핫플',
+    '스타벅스',
+    '힐링',
+    '자연',
+    '오솔길',
+    '도심',
+    '출근길',
+    '퇴근길',
+    '점심시간',
+    '스트레스해소',
+    '한강',
+    '공원',
+    '바다',
+    '해안가',
+    '개울가',
+    '계곡',
+    '들판',
+    '산',
+    '동산',
+    '숲길',
+    '러닝',
+    '맛집',
+    '카페',
+    '영화',
+    '문화',
+    '사색',
+    '문화재',
+    '강아지',
+    '고양이',
+    '야경',
+    '노을',
+  ];
   List<Map<String, String>> keywordStatus = [];
 
   // 산책로 정보 업데이트
@@ -37,8 +99,13 @@ class _CourseEditpageState extends State<CourseEditpage> {
             })
         .toList();
     final response = await apiService.putRequest(
-        'course/enrollment/${widget.id}',
-        {'title': newTitle, 'introduction': newIntro, 'tags': tags});
+      'course/enrollment/${widget.id}',
+      {
+        'title': newTitle,
+        'introduction': newIntro,
+        'tags': tags,
+      },
+    );
     // 산책로 정보 다시 불러오기
   }
 
@@ -47,12 +114,12 @@ class _CourseEditpageState extends State<CourseEditpage> {
     super.initState();
     newIntro = widget.introduction;
     newTitle = widget.title;
-
     // Initialize keyword status
-    for (final keyword in widget.keywords) {
+    for (final keyword in allKeywords) {
+      final status = widget.keywords.contains(keyword) ? 'DEFAULT' : 'DELETE';
       keywordStatus.add({
         'name': keyword,
-        'status': 'DEFAULT',
+        'status': status,
       });
     }
   }
@@ -64,7 +131,9 @@ class _CourseEditpageState extends State<CourseEditpage> {
           if (item['status'] == 'DEFAULT') {
             item['status'] = 'DELETE';
           } else if (item['status'] == 'DELETE') {
-            item['status'] = 'DEFAULT';
+            item['status'] = 'NEW';
+          } else if (item['status'] == 'NEW') {
+            item['status'] = 'DELETE';
           }
           break;
         }
@@ -106,151 +175,102 @@ class _CourseEditpageState extends State<CourseEditpage> {
             TextButton(
               onPressed: () {
                 saveChanges();
-                Navigator.of(context).pop(); // 이전 페이지 (공개 산책로 디테일 페이지)로 돌아가기
+                Navigator.of(context).pop(); // 이전 페이지로 이동
               },
               child: const Text(
-                '완료', // 완료 버튼을 누르면 모두 저장
+                '저장',
                 style: TextStyle(
+                  fontSize: 16,
                   color: Colors.black,
-                  fontSize: 20.0,
                   fontWeight: FontWeight.w600,
                 ),
               ),
             ),
-            const SizedBox(width: 6),
           ],
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Column(
-                children: [
-                  const Divider(
-                    thickness: 1,
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          initialValue: widget.title,
-                          onChanged: (value) {
-                            // 산책로 제목 업데이트
-                            setState(() {
-                              newTitle = value;
-                            });
-                          },
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.center,
-                          decoration: const InputDecoration(
-                            hintText: '제목',
-                            border: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        children: [
+          const SizedBox(height: 16.0),
+          const SizedBox(height: 16.0),
+          const Text(
+            '제목',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
             ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Column(
-                children: [
-                  const Divider(
-                    thickness: 1,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          initialValue: widget.introduction,
-                          onChanged: (value) {
-                            // 산책로 소개 업데이트
-                            setState(() {
-                              newIntro = value;
-                            });
-                          },
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.center,
-                          decoration: const InputDecoration(
-                            hintText: '소개',
-                            border: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+          ),
+          const SizedBox(height: 8.0),
+          TextFormField(
+            initialValue: widget.title,
+            onChanged: (value) {
+              setState(() {
+                newTitle = value;
+              });
+            },
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: '산책로 제목을 입력하세요.',
             ),
-            const SizedBox(height: 20),
-            // Keyword Selection
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Divider(
-                    thickness: 1,
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    '키워드',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Wrap(
-                    spacing: 8.0,
-                    runSpacing: 8.0,
-                    children: widget.keywords
-                        .map(
-                          (keyword) => GestureDetector(
-                            onTap: () => toggleKeywordStatus(keyword),
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 4.0, horizontal: 10.0),
-                              decoration: BoxDecoration(
-                                color: keywordStatus.firstWhere((item) =>
-                                            item['name'] ==
-                                            keyword)['status'] ==
-                                        'DEFAULT'
-                                    ? Colors.blue
-                                    : Colors.black,
-                                borderRadius: BorderRadius.circular(16.0),
-                              ),
-                              child: Text(
-                                keyword,
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                        )
-                        .toList(),
-                  ),
-                ],
-              ),
+          ),
+          const SizedBox(height: 16.0),
+          const Text(
+            '소개',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 8.0),
+          TextFormField(
+            initialValue: widget.introduction,
+            onChanged: (value) {
+              setState(() {
+                newIntro = value;
+              });
+            },
+            maxLines: 5,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: '산책로에 대한 소개를 입력하세요.',
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          const Text(
+            '키워드',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(height: 8.0),
+          Wrap(
+            spacing: 8.0,
+            runSpacing: 4.0,
+            children: allKeywords.map((keyword) {
+              final keywordItem = keywordStatus.firstWhere(
+                (item) => item['name'] == keyword,
+                orElse: () => {'name': keyword, 'status': 'DELETE'},
+              );
+              final isSelected = (keywordItem['status'] == 'DEFAULT' ||
+                  keywordItem['status'] == 'NEW');
+              return ChoiceChip(
+                label: Text(keyword),
+                selected: isSelected,
+                onSelected: (isSelected) {
+                  toggleKeywordStatus(keyword);
+                },
+                selectedColor: isSelected ? Colors.green : null,
+                labelStyle: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w600,
+                  color: isSelected ? Colors.white : Colors.black,
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
