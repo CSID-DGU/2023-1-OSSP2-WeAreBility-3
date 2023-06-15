c83d3ceca70d91e97d4e7956b3b4b2096c7ff869
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-06-15 12:13:30 +0900

    feat : 산책로 배너 디테일

--- frontend/naemansan/lib/main.dart
+++ frontend/naemansan/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk_talk.dart';
 import 'package:naemansan/providers/location.dart';
+import 'package:naemansan/screens/Home/banner_detail_screen.dart';
 import 'package:naemansan/screens/login_screen.dart';
 import 'package:naemansan/screens/map/create_title_map.dart';
 import 'package:naemansan/screens/screen_index.dart';
@@ -102,7 +103,36 @@ class _AppState extends State<App> {
         } else {
           return MaterialApp(
             title: '내가 만든 산책로',
-            home: isLogged ? const IndexScreen(index: 0) : const LoginScreen(),
+            home: isLogged
+                ? const IndexScreen(index: 0)
+                : const BannerDetailScreen(
+                    caption: "내가 만든 산책로, 내만산이란?",
+                    content: """
+안녕하세요!\n
+\n
+내가 만드는 산책로, 내만산입니다.\n
+\n
+내만산은 양방향 산책로 공유 서비스 지원함으로서 ㄱ\n
+개인만의 산책로를 알릴 수 있는 플랫폼입니다.\n
+\n
+[title]나만의 산책로 생성\n
+\n
+나만의 산책로를 만들어봐요!\n
+나만의 산책로 -> 상단 + 아이콘을 클릭하면 나만의 산책로를 만들 수 있답니다.\n
+직접 산책해야 길을 생성할 수 있습니다!\n
+\n
+[title]모두의 산책로\n
+\n
+나만의 산책로를 공유해봐요!\n
+나만의 산책로를 공유함으로서 나만의 산책로를 더 널리알릴 수 있고 \n
+다른이의 산책로를 이용함으로서 다양한 산책로들을 알아갈 수 있어요! 
+\n
+[title]맞춤형 산책로 추천\n
+\n
+공개등록된 산책로를 위치, 키워드 별로 추천받을 수 있습니다.\n
+이를 통해 더 다양한 산책로들을 구경하고 이용하는\n
+시간을 가져봐요 :)\n""",
+                  ),
             routes: {
               '/index': (context) => const IndexScreen(index: 0),
               '/login': (context) => const LoginScreen(),
